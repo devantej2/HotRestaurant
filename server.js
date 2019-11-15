dda0c0838366cb4a799bf047bf1823ecedc0aa8d
Devante <devante.jones229@gmail.com>
2019-11-15 11:52:45 -0500

    removed script src for server in home.html, added GET request to tables page and reservation page

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get('/tables', function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/reserve', function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+
 app.listen(port, function (){
     console.log(`Example app listening on port port!`);
 });
