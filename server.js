baef00fa2461e29b74bfdd80b018992c0e48646c
Devante <devante.jones229@gmail.com>
2019-11-15 11:28:57 -0500

    created route to home.html and added script source to home.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.listen(port, function (){
